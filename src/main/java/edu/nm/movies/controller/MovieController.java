105c6b1a74e1b44b3c1605056f83290c2d07c7fa
NikMassV <slipknoshka777@gmail.com>
2024-02-01 12:52:32 +0100

    migrate db and add cross origin

--- src/main/java/edu/nm/movies/controller/MovieController.java
+++ src/main/java/edu/nm/movies/controller/MovieController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(allowedHeaders = "*", origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
